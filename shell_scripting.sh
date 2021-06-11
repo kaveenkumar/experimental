6397d1cd516cef2f463a7edc9f8677b14fd14cf0
Kaveen Kumar <2idli1vada@email.com>
2021-06-11 19:45:50 +0200

    Update shell_scripting.sh

--- shell_scripting.sh
+++ shell_scripting.sh
@@ -1,9 +1,8 @@
 if [ $stage -le 0 ]; then
   echo "$0: startig stage 0"
   # pass args to python script
-  python3 mix_noise_to_audio.py \
-          ./db/recordings \
-          ./db/skdy_silence.wav \y
-          ./db/noise \
-          $snr
+  python3 foo.py \
+          ./db/data \
+          ./db/music.wav \
+          $val
 fi
