8e0ef494174591f4fa092f72747102e5e0a2c8bd
Kaveen Kumar <2idli1vada@email.com>
2021-06-11 19:46:26 +0200

    Update shell_scripting.sh

--- shell_scripting.sh
+++ shell_scripting.sh
@@ -1,8 +1,10 @@
+stage=0
+
 if [ $stage -le 0 ]; then
-  echo "$0: startig stage 0"
+  echo "$0: starting stage 0"
   # pass args to python script
   python3 foo.py \
           ./db/data \
           ./db/music.wav \
-          $val
+          $var
 fi
