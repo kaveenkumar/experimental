166c9248e2f5b4de24fec3d7fb70fcfc2fd15502
Kaveen Kumar <2idli1vada@email.com>
2021-06-11 19:45:08 +0200

    Update shell_scripting.sh

--- shell_scripting.sh
+++ shell_scripting.sh
@@ -1,6 +1,6 @@
 if [ $stage -le 0 ]; then
-  echo "$0: mixing noise to clean audio"
-  # args passed to the mix_noise_to_audio.py script: folder of clean audio, file of formfactor noise, folder of noise, snr value
+  echo "$0: startig stage 0"
+  # pass args to python script
   python3 mix_noise_to_audio.py \
           ./db/recordings \
           ./db/skdy_silence.wav \y
