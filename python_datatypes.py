727be3921ec933a71509243a986ebcd1a2a0facc
Kaveen Kumar <2idli1vada@email.com>
2021-06-20 11:02:49 +0200

    Create python_datatypes.py

---
+++ python_datatypes.py
@@ -0,0 +1,5 @@
+    "- List is a collection which is ordered and mutable. Allows duplicate members.\n",
+    "- Tuple is a collection which is ordered and immutable. Allows duplicate members.\n",
+    "- Set is a collection which is unordered and unindexed. No duplicate members.\n",
+    "- Dictionary is a collection which is unordered, mutable and indexed. No duplicate members.\n",
+    "- Strings are immutable sequences of Unicode code points."
