3c8ef11cfe69e7f55c57b5b3fc0d1ce75b77575b
Kaveen Kumar <2idli1vada@email.com>
2021-06-20 11:05:27 +0200

    Update python_datatypes.py

--- python_datatypes.py
+++ python_datatypes.py
@@ -1,7 +1,7 @@
 ### primitive datatypes
 
-    "- List is a collection which is ordered and mutable. Allows duplicate members.\n",
-    "- Tuple is a collection which is ordered and immutable. Allows duplicate members.\n",
-    "- Set is a collection which is unordered and unindexed. No duplicate members.\n",
-    "- Dictionary is a collection which is unordered, mutable and indexed. No duplicate members.\n",
-    "- Strings are immutable sequences of Unicode code points."
+- List is a collection which is ordered and mutable. Allows duplicate members.
+- Tuple is a collection which is ordered and immutable. Allows duplicate members.
+- Set is a collection which is unordered and unindexed. No duplicate members.
+- Dictionary is a collection which is unordered, mutable and indexed. No duplicate members.
+- Strings are immutable sequences of Unicode code points.
