0abae70ff915509f8848e67f6c4a581ecfb929cb
Kaveen Kumar <2idli1vada@email.com>
2021-07-05 14:55:41 +0200

    py import related codes

---
+++ import_modules.py
@@ -0,0 +1,3 @@
+echo $PATH  # on a terminal, returns the paths that the os looks at for programs
+export PATH='/User/bin/dir:$PATH'  # add your own path to os path environment
+source .bashrc | restart terminal
