da75ceaec88c6af13553e1e529f26354e4b6c0b6
Kaveen Kumar <2idli1vada@email.com>
2021-08-25 14:08:49 +0200

    initiate file

---
+++ text_translator.py
@@ -0,0 +1,9 @@
+"""Translator api for converting text from one language to another language;
+Uses https://pypi.org/project/translate/ in the backend
+
+Different translation providers available:
+    'mymemory': MyMemoryProvider,
+    'microsoft': MicrosoftProvider,
+    'deepl': DeeplProvider,
+    'libre': LibreProvider,
+"""
