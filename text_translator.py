52825743f1cae96c086de8be45ea146add98e23f
Kaveen Kumar <2idli1vada@email.com>
2021-08-25 14:09:37 +0200

    main function

--- text_translator.py
+++ text_translator.py
@@ -10,5 +10,19 @@ Different translation providers available:
 
 from translate import Translator
 
+def main():
+    """main function for translation"""
+    source_text = "Guten Morgen"  # input text to be translated
+    source_language = ''  # input text language
+    destination_language = 'en'  # output text language
+    translation_provider = 'mymemory'  # refer to docstring for all available providers
+    base_url = """https://translate.astian.org/"""  # necessary if the provider is libre
+
+    translator = TextTranslator(src_lang=source_language, dst_lang=destination_language, provider=translation_provider, base_url=base_url)
+    destination_text = translator.translate(source_text)
+
+    print(destination_text)
+    
+
 if __name__ == '__main__':
     main()
