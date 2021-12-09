34134d35331d5018b5de5fe137f4bf7f7a036f28
Kaveen Kumar <2idli1vada@email.com>
2021-12-09 09:33:30 +0100

    py dates

--- py_dates.py
+++ py_dates.py
@@ -60,3 +60,6 @@ print(dt_mtn.strftime('%B %d, %Y'))
 dt_str = 'July 24, 2016'
 dt = datetime.datetime.strptime(dt_str, '%B %d, %Y')
 print(dt)
+
+# strftime - Datetime to String
+# strptime - String to Datetime
