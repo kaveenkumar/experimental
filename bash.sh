d3416cdc1537def1110c9fefd13ab4610a183688
Kaveen Kumar <2idli1vada@email.com>
2022-07-14 16:04:22 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -24,7 +24,23 @@ chmod +x bash.sh
 sleep 1
 -> sleep for 1 sec
 
+name="Kaveen"
+-> name is a variable
+echo "Hello $name"
 
+echo "What is your name?"
+read name
+-> allows argument passing via terminal (similar to input() in python)
+echo "Hello $name"
+
+name=$1
+-> first argument passed
+./bash.sh Kaveen
+
+name=$1 age=$2
+
+user=$(whoami)
+-> format for variable = output of a command
 
 
 
