2d8a46c6a9a980871ca9872b6809fbac53cd6aba
Kaveen Kumar <2idli1vada@email.com>
2022-07-14 16:03:58 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,40 @@
+which $SHELL
+-> /bin/bash
+
+echo $USER
+echo $HOSTNAME
+echo $PWD
+-> another way of running the commands
+
+#!/bin/bash
+-> called a shebang
+
+whoami
+-> user name
+
+date
+-> current date and time
+
+bash bash.sh
+OR
+chmod +x bash.sh
+./bash.sh
+-> to run the bash script
+
+sleep 1
+-> sleep for 1 sec
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
