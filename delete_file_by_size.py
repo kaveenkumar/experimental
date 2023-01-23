04c80386b8d48c9070561358d7035178da15a831
Kaveen Kumar <2idli1vada@email.com>
2023-01-23 14:42:11 +0100

    Update delete_file_by_size.py

--- delete_file_by_size.py
+++ delete_file_by_size.py
@@ -2,5 +2,5 @@ from pathlib import Path
 folder = "/home/kaveen/datasets/en/"
 
 for path in Path(folder).rglob('*.wav'):
-    if os.path.getsize(path) < 10 * 1024:
+    if os.path.getsize(path) < 10 * 1024:  #file size in kb
         os.remove(path)
